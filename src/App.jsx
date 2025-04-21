8e827e83bb93b3c0f4a2ac1b2a2584b809e85e8d
Ajith PM <ajithpmdwaraka@gmail.com>
2025-04-21 10:23:01 +0530

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react"
 import "./App.css"
 import supabase from "./supabase"
+import { useEffect } from "react"
 
 function App() {
   const [todoList, setTodoList] = useState([])
   const [newTodo, setNewTodo] = useState("")
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from("TodoList").select("*")
+    if (error) {
+      console.log("Error fetching data", error)
+    } else {
+      setTodoList(data)
+    }
+  }
 
   const addToDo = async () => {
     const newTodoData = {
@@ -15,14 +29,14 @@ function App() {
       .from("TodoList")
       .insert([newTodoData])
       .single()
-    
+
     if (error) {
-      console.log("Error adding todo", error);
+      console.log("Error adding todo", error)
     } else {
-      setTodoList((prev) => [...prev, data]);
-      setNewTodo("");
+      setTodoList((prev) => [...prev, data])
+      setNewTodo("")
     }
-  };
+  }
 
   return (
     <>
@@ -33,11 +47,19 @@ function App() {
         <input
           type="text"
           placeholder="New ToDo"
+          value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
         />
         <button onClick={addToDo}>Add Todo Items </button>
       </div>
-      <ul></ul>
+      <ul>
+        {todoList.map((todo) => (
+          <li>
+            <p>{todo.name}</p>
+            <button> {todo.isCompleted ? "Undo" : "Complete Task"}</button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
